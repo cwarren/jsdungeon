a1a5fa6f83aef5b15a44482600cf628df332bee4
Chris <csw11235@gmail.com>
2025-04-15 23:49:03 -0400

    add link to github pages in repo readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # jsdungeon
 
 A basic rogue-like dungeon crawler
+
+Playable at https://cwarren.github.io/jsdungeon/playgame.html
--- item/itemDefinitions.js
+++ item/itemDefinitions.js
@@ -1,5 +1,7 @@
 import { EffGenDamage } from "../effect/effGenDamageClass.js";
 
+// NOTE: it's fine for weight and volume to be 0 or fractional/decimal values; negative would be weird, but should work too
+
 const ITEM_DEFINITIONS = [
   {
     type: "ROCK", name: "Rock", description: "It's a rock. It would be a poor gift.", displaySymbol: "*", displayColor: "#fff", weight: 5, volume: 1,
